444c8af162e0d88682cd0f9d229411d3cd9003d0
same-jima <mba10penguin@icloud.com>
2021-07-20 14:41:18 +0900

    多数修正_新規登録には、パスワードは確認用を含めて2回入力すること。コメント削除機能アクションのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable
+  :recoverable, :rememberable, :validatable
+
 
   validates :email, presence: true
   validates :name, presence: true
